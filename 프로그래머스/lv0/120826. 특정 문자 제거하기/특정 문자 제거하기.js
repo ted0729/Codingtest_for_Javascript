cd98dd8789413b904a1e4d594194249d71e47932
ted0729 <138233242+ted0729@users.noreply.github.com>
2023-08-28 16:32:39 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,12 @@
+function solution(my_string, letter) {
+    const answer = my_string.replaceAll(letter, '');
+    
+    return answer;
+}
+
+// function solution(my_string, letter) {
+    
+//     const answer = my_string.split(letter).join('')
+    
+//     return answer;
+// }
