e4a5fc0a11e9de6775399feba5165e599cbb6e41
ted0729 <138233242+ted0729@users.noreply.github.com>
2023-08-28 16:24:05 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -0,0 +1,11 @@
+function solution(slice, n) {
+    var answer = 0;
+    
+    if (n % slice === 0) {
+        answer = n / slice;
+    } else {
+        answer = parseInt(n / slice + 1);
+    }
+    
+    return answer;
+}
