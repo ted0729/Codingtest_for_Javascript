d6ef27b8785d6d4e950c05272e71ed51613cf5a8
Taehyun Kim <138233242+ted0729@users.noreply.github.com>
2023-09-23 09:12:11 +0900

    [level 0] Title: A로 B 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,12 @@
+function solution(before, after) {
+    var answer = 0;
+    const sortedBefore = before.split('').sort().join('');
+    const sortedAfter = after.split('').sort().join('');
+
+    // 정렬된 문자열이 동일하면 1을 반환, 아니면 0을 반환
+    if (sortedBefore === sortedAfter) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
