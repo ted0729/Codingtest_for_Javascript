5153885cd94a7a8aea0aff9dea2b3b8298f5c226
Taehyun Kim <138233242+ted0729@users.noreply.github.com>
2024-01-10 15:33:39 +0900

    [level unrated] Title: 수 조작하기 2, Time: 6.84 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,24 @@
+function solution(numLog) {
+    let answer = '';
+
+    for (let i = 1; i < numLog.length; i++) {
+        const diff = numLog[i] - numLog[i - 1];
+
+        switch (diff) {
+            case 1:
+                answer += 'w';
+                break;
+            case -1:
+                answer += 's';
+                break;
+            case 10:
+                answer += 'd';
+                break;
+            case -10:
+                answer += 'a';
+                break;
+        }
+    }
+
+    return answer;
+}
